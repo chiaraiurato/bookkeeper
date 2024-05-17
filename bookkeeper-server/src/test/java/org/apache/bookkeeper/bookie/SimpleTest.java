965d6aea37b0e007492ce0edddf0bac4027c9e7f
chiaraiurato <chiara.iurat@gmail.com>
2024-05-17 14:54:02 +0200

    First try on Jacoco plugin
    Added Jacoco
    Added SonarCloud
    Created aSimpleTest

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/SimpleTest.java
@@ -0,0 +1,14 @@
+package org.apache.bookkeeper.bookie;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleTest {
+    @Test
+    public void aSimpleTest(){
+        int a=1;
+        int b=2;
+
+        Assert.assertEquals(3, a+b);
+    }
+}
